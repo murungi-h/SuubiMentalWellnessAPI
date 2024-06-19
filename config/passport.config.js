d54b681dd3ad1e193ca282fb24dcfcb53a7be8a2
murungi <127125055+murungi-h@users.noreply.github.com>
2024-06-20 00:30:56 +0300

    Added passport config

--- config/passport.config.js
+++ config/passport.config.js
@@ -1,49 +1,152 @@
-const express = require('express');
-const passport = require('passport')
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local').Strategy;
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const MicrosoftStrategy = require('passport-microsoft').Strategy;
-const { localStrategy, googleStrategy, facebookStrategy, microsoftStrategy } = require('../controller/authController');
+const User = require('../models/userModel');
+require('dotenv')
 
-//Passport Strategy configuration
+module.exports = function(passport) {
+  // JWT Strategy
+  const opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+  opts.secretOrKey = keys.secretOrKey;
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
 
-//Local strategy
-passport.use(new LocalStrategy({ usernameField: 'email'}, localStrategy));
+  // Local Strategy
+  passport.use(
+    new LocalStrategy(
+      { usernameField: 'email' },
+      async (email, password, done) => {
+        try {
+          // Find the user by email
+          const user = await User.findOne({ email });
 
-//Google strategy
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
-}, googleStrategy));
+          // If user not found
+          if (!user) {
+            return done(null, false, { message: 'Invalid email or password' });
+          }
 
-//Facebook strategy
-passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: '/auth/facebook/callback'
-}, facebookStrategy));
+          // Verify the password
+          const isMatch = await user.comparePassword(password);
 
-//Microsoft strategy
-passport.use(new MicrosoftStrategy({
-    clientID: process.env.MICROSOFT_CLIENT_ID,
-    clientSecret: process.env.MICROSOFT_CLIENT_SECRET,
-    callbackURL: '/auth/microsoft/callback'
-}, microsoftStrategy));
+          // If password is incorrect
+          if (!isMatch) {
+            return done(null, false, { message: 'Invalid email or password' });
+          }
 
-//Serialize and Deserialize user
-passport.serializeUser((user, done) => done(null, user.id));
+          // Return the user if everything is valid
+          return done(null, user);
+        } catch (err) {
+          console.error(err);
+          return done(err);
+        }
+      }
+    )
+  );
 
-passport.deserializeUser(async function(id, done){
-    try {
-        const user = await User.findById(id);
-        done(null, user);
-    } catch (error) {
-        done(error);
-    }
-});
+  // Google Strategy
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/api/auth/google/callback'
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          // Find or create the user
+          let user = await User.findOne({ googleId: profile.id });
 
-module.exports = {
-    passportConfig: passport
+          if (!user) {
+            user = new User({
+              googleId: profile.id,
+              email: profile.emails[0].value,
+              // Add any other required fields
+            });
+            await user.save();
+          }
+
+          return done(null, user);
+        } catch (err) {
+          console.error(err);
+          return done(err);
+        }
+      }
+    )
+  );
+
+  // Facebook Strategy
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: keys.facebookClientID,
+        clientSecret: keys.facebookClientSecret,
+        callbackURL: '/api/auth/facebook/callback'
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          // Find or create the user
+          let user = await User.findOne({ facebookId: profile.id });
+
+          if (!user) {
+            user = new User({
+              facebookId: profile.id,
+              email: profile.emails[0].value,
+              // Add any other required fields
+            });
+            await user.save();
+          }
+
+          return done(null, user);
+        } catch (err) {
+          console.error(err);
+          return done(err);
+        }
+      }
+    )
+  );
+
+  // Microsoft Strategy
+  passport.use(
+    new MicrosoftStrategy(
+      {
+        clientID: keys.microsoftClientID,
+        clientSecret: keys.microsoftClientSecret,
+        callbackURL: '/api/auth/microsoft/callback'
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          // Find or create the user
+          let user = await User.findOne({ microsoftId: profile.id });
+
+          if (!user) {
+            user = new User({
+              microsoftId: profile.id,
+              email: profile.emails[0].value,
+              // Add any other required fields
+            });
+            await user.save();
+          }
+
+          return done(null, user);
+        } catch (err) {
+          console.error(err);
+          return done(err);
+        }
+      }
+    )
+  );
 };
